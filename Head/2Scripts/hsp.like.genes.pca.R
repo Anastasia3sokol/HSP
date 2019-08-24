393db47cadd6a2a9aaf822b0a757bb80a0193cce
ValeraKus <valeratimonina@gmail.com>
2019-08-24 21:12:36 +0200

    pca to find genes comparable with hsp90

--- Head/2Scripts/hsp.like.genes.pca.R
+++ Head/2Scripts/hsp.like.genes.pca.R
@@ -15,13 +15,22 @@ genes_without_na = genes_without_na[!genes_without_na$EnsemblId %in% DuplGenes,]
 genes_pca <- prcomp(genes_without_na[,-c(1,2)], scale. = T)
 #plot(genes_pca)
 
-pdf('../../Body/4_Figures/pca_HSP_like_genes.pdf')
-biplot(genes_pca, choices = 1:2, pc.biplot = F, col = c('white', 'black'), cex = 0.6, main = 'PCA HSP like genes', xlim = c(-0.08,0.08)) # очень клевая картинка!!!!
+pdf('../../Body/4_Figures/hsp.like.genes.pca.pdf')
+biplot(genes_pca, choices = 1:2, pc.biplot = F, col = c('white', 'black'), cex = 0.6, main = 'PCA HSP like genes', xlim = c(-0.08,0.08), xlab = 'PC1 (24% explaned var.)', ylab = 'PC2 (13.4% explaned var.)' ) # очень клевая картинка!!!!
+
 biplot(genes_pca, choices = 2:3, pc.biplot = F, col = c('white', 'black'), cex = 0.6, main = 'PCA HSP like genes', xlim = c(-0.08,0.08))
+
+plot(genes_PC$PC1, genes_PC$PC2, col = 'darkslategray3', xlab = 'PC1 (24% explaned var.)', ylab = 'PC2 (13.4% explaned var.)', main = 'Subset of genes similar to HSP90', asp = 1)
+points(genes_like_hsp$PC1, genes_like_hsp$PC2, col = 'red')
+points(genes_like_hsp[genes_like_hsp$EnsemblId == HSP_id,'PC1'], genes_like_hsp[genes_like_hsp$EnsemblId == HSP_id,'PC2'], col = 'black', pch = '*')
+legend(x = 2, y = 11, legend = c('All genes', 'Subset of genes closed \n to hsp90', 'hsp90'), col = c('darkslategray3', 'red', 'black'), pch = c('o', 'o', '*'))
+
 dev.off()
 
 PC1 <- predict(genes_pca)[,1]
 PC2 <- predict(genes_pca)[,2]
+PC1_var <- summary(genes_pca)$importance[2]
+PC2_var <- summary(genes_pca)$importance[5]
 genes_labels <- genes_without_na[,c(1,2)]
 
 genes_PC <- data.frame(genes_labels, PC1, PC2)
@@ -31,15 +40,8 @@ HspPc2 <- genes_PC[genes_PC$EnsemblId == HSP_id, 4]
 # надо найти 300 генов - которые в маленьком кружке с центром - Hsp90 и в идеале проранжировать их по степени схожести (надо повторить геометрию - расстояние между точками...)
 
 genes_PC$distance_to_hsp <- sqrt((HspPc1-genes_PC$PC1)**2 + (HspPc2-genes_PC$PC2)**2)
-genes_like_hsp <- genes_PC[order(genes_PC$distance_to_hsp),][1:300,]
-write.table(genes_like_hsp, '../../Body/2_Derived/genes.ranged.by.distance.to.hsp.txt')
+genes_like_hsp <- genes_PC[order(genes_PC$distance_to_hsp),]
+write.table(genes_like_hsp, '../../Body/2_Derived/hsp.like.genes.pca.genes.ranged.by.distance.to.hsp.txt')
 
 
 
-pdf('../../Body/4_Figures/HSP_like_genes_subset.pdf')
-par(mfcol = c(1,1))
-plot(genes_PC$PC1, genes_PC$PC2, col = 'darkslategray3', xlab = 'PC1', ylab = 'PC2', main = 'Subset of genes similar to HSP90', asp = 1)
-points(genes_like_hsp$PC1, genes_like_hsp$PC2, col = 'red')
-points(genes_like_hsp[genes_like_hsp$EnsemblId == HSP_id,'PC1'], genes_like_hsp[genes_like_hsp$EnsemblId == HSP_id,'PC2'], col = 'black', pch = '*')
-legend(x = 2, y = 11, legend = c('All genes', 'Subset of genes closed \n to hsp90', 'hsp90'), col = c('darkslategray3', 'red', 'black'), pch = c('o', 'o', '*'))
-dev.off()
