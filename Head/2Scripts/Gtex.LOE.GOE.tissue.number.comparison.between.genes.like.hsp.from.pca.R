e057f2c01e5610fdd579ebd4f63a329bd786b646
ValeraKus <valeratimonina@gmail.com>
2019-09-05 16:43:15 +0200

    count LOE and GOE cis-eQTLs in each gene

--- Head/2Scripts/Gtex.LOE.GOE.tissue.number.comparison.between.genes.like.hsp.from.pca.R
+++ Head/2Scripts/Gtex.LOE.GOE.tissue.number.comparison.between.genes.like.hsp.from.pca.R
@@ -4,20 +4,27 @@ tissue_num <- read.table('../../Body/2_Derived/Gtex.LOE.GOE.tissues.numbers.txt'
 gene <- gsub('.*b37_','', tissue_num$cis_eQTL_id)
 tissue_num <- cbind(tissue_num, gene)
 
-x <- c()
-y <- c()
+LOE_tissues <- c()
+LOE_cis_eQTLs <- c()
+GOE_tissues <- c()
+GOE_cis_eQTLs <- c()
 unique_genes <- unique(gene)
 for (g in unique_genes){
-  loe <- sum(tissue_num[tissue_num$gene == g,'LOE_tissues'])
-  goe <- sum(tissue_num[tissue_num$gene == g,'GOE_tissues'])
-  x <- c(x,loe)
-  y <- c(y,goe)
-  
+  #count common amount of tissues for each gene in which there is cis-eQTLs
+  loe_tis <- sum(tissue_num[tissue_num$gene == g,'LOE_tissues'])
+  goe_tis <- sum(tissue_num[tissue_num$gene == g,'GOE_tissues'])
+  LOE_tissues <- c(LOE_tissues,loe_tis)
+  GOE_tissues <- c(GOE_tissues,goe_tis)
+  #count common amount of unique cis-eQTLs for each gene
+  loe_eqtl <- dim(tissue_num[(tissue_num$gene == g) & (tissue_num$LOE_tissues > 0),])[1]
+  goe_eqtl <- dim(tissue_num[(tissue_num$gene == g) & (tissue_num$GOE_tissues > 0),])[1]
+  LOE_cis_eQTLs <- c(LOE_cis_eQTLs, loe_eqtl)
+  GOE_cis_eQTLs <- c(GOE_cis_eQTLs, goe_eqtl)
 }
 
 pdf('../../Body/4_Figures/Gtex.LOE.vs.GOE.all.tissues.genes.like.hsp.pdf')
-plot(x,y, asp = 1, xlab = 'LOE cis-eQTL sum in all tissue', ylab = 'GOE cis-eQTL sum in all tissue')
-points(x[which(unique_genes == 'ENSG00000096384')], y[which(unique_genes == 'ENSG00000096384')], col = 'red', pch = 19)
+plot(LOE_tissues,GOE_tissues, asp = 1, xlab = 'LOE cis-eQTL sum in all tissue', ylab = 'GOE cis-eQTL sum in all tissue')
+points(LOE_tissues[which(unique_genes == 'ENSG00000096384')], GOE_tissues[which(unique_genes == 'ENSG00000096384')], col = 'red', pch = 19)
 title('LOE vs GOE in all tissues')
 dev.off()
 
@@ -28,23 +35,14 @@ boxplot(tissue_num[tissue_num$gene == 'ENSG00000096384','LOE_tissues'], tissue_n
 title('LOE cis-eQTL tissue number')
 dev.off()
 
-hsp90 <- as.numeric(tissue_num$gene == 'ENSG00000096384')
-tissue_num <- cbind(tissue_num, hsp90)
-
-library(ggplot2)
-
-library(rcompanion)
-Sum = groupwiseMean(LOE_tissues ~ hsp90,
-                    data   = tissue_num,
-                    conf   = 0.95,
-                    digits = 3)
-qplot(x    = hsp90 , 
-      y    = Mean,
-      data = Sum) +
-  
-  geom_errorbar(aes( 
-    ymin  = Trad.lower, 
-    ymax  = Trad.upper, 
-    width = 0.15))
+plot(LOE_cis_eQTLs, GOE_cis_eQTLs)
+points(LOE_cis_eQTLs[which(unique_genes == 'ENSG00000096384')], GOE_cis_eQTLs[which(unique_genes == 'ENSG00000096384')], col = 'red', pch = 19)
 
 
+boxplot(LOE_cis_eQTLs, GOE_cis_eQTLs, names = c('LOE', 'GOE'))
+
+
+boxplot(GOE_cis_eQTLs/LOE_cis_eQTLs)
+points(GOE_cis_eQTLs[which(unique_genes == 'ENSG00000096384')]/LOE_cis_eQTLs[which(unique_genes == 'ENSG00000096384')], col = 'red', pch = 19)
+boxplot(GOE_tissues/LOE_tissues)
+points(GOE_tissues[which(unique_genes == 'ENSG00000096384')]/LOE_tissues[which(unique_genes == 'ENSG00000096384')], col = 'red', pch = 19)
--- Head/2Scripts/Gtex.LOE.GOE.tissue.numbers.R
+++ Head/2Scripts/Gtex.LOE.GOE.tissue.numbers.R
@@ -1,14 +1,29 @@
 rm(list=ls(all=TRUE))
 All_best_var <- read.table('../../Body/2_Derived/All.best.vat.all.tissue.variants.outside.genes.important.columns.txt')
 
-genes.like.hsp <- as.vector(read.table('../../Body/2_Derived/genes.ranged.by.distance.to.hsp.txt')[1:300,1])
+genes.like.hsp <- as.vector(read.table('../../Body/2_Derived/hsp.like.genes.pca.genes.ranged.by.distance.to.hsp.txt')[1:300,1])
 All_best_var_genes_like_hsp <- All_best_var[grepl(paste(genes.like.hsp, collapse="|"), All_best_var$cis_eQTL_id),]
 
+#look at hsp90
+All_best_var_hsp <- All_best_var_genes_like_hsp[grepl('ENSG00000096384', All_best_var_genes_like_hsp$cis_eQTL_id),]
+length(All_best_var_hsp$cis_eQTL_id) == length(unique(All_best_var_hsp$cis_eQTL_id)) #TRUE 47
+length(All_best_var_hsp$Tissue) == length(unique(All_best_var_hsp$Tissue)) #TRUE 47
+# one cis-eQTL in one tissue
+hsp_LOE <- All_best_var_hsp[All_best_var_hsp$slope < 0, 'Assessed_Allele_Freq']
+hsp_GOE <- All_best_var_hsp[All_best_var_hsp$slope > 0, 'Assessed_Allele_Freq']
+
+x <- c(hsp_GOE, hsp_LOE)
+gp = c(rep("hsp90 GOE Assessed allele freq", length(hsp_GOE)),rep("hsp90 LOE Assessed allele freq",length(hsp_LOE)))
+boxplot(x ~ gp, xlab = '')
+t.test(hsp_GOE, hsp_LOE) #no difference p-value = 0.7528
+
+
+# count tissues in which each unique cis-eQTL is occured
 cis_eQTL_id <- as.array(unique(All_best_var_genes_like_hsp$cis_eQTL_id))
 LOE_tissues <- c()
 GOE_tissues <- c()
 
-#безумно долгий цикл
+
 for (QTL in cis_eQTL_id){
   LOE_count <- 0
   LOE <- All_best_var_genes_like_hsp[(All_best_var_genes_like_hsp$cis_eQTL_id == QTL) & (All_best_var_genes_like_hsp$slope < 0),]$Tissue
@@ -24,3 +39,6 @@ for (QTL in cis_eQTL_id){
 cis_eQTL_id <- as.vector(cis_eQTL_id)
 tissue_numbers <- data.frame(cbind(cis_eQTL_id, LOE_tissues, GOE_tissues))
 write.table(tissue_numbers, '../../Body/2_Derived/Gtex.LOE.GOE.tissues.numbers.txt')
+
+
+
